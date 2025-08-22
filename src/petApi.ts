e19baf0651c9212c593bcd58d9097cb09103d781
Casper <casfro01@easv365.dk>
2025-08-22 12:05:01 +0200

    delete pets

--- src/petApi.ts
+++ src/petApi.ts
@@ -2,4 +2,5 @@ const baseUrl = 'https://api-divine-grass-2111.fly.dev'
 export const petApi = {
     getPetsUrl: baseUrl + '/GetPets',
     getCreateUrl: baseUrl + '/CreatePet',
+    getDeleteUrl: baseUrl + '/DeletePet',
 }
